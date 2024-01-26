0eb463750681edcf6beb65e744641e23ef19d713
Max ist gay XaXa <71442255+Flexflext@users.noreply.github.com>
2024-01-26 21:49:47 +0100

    Init

---
+++ ProjectWonky/Source/ProjectWonky/ProjectWonkyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectWonkyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectWonkyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectWonkyGameMode();
+};
+
+
+
